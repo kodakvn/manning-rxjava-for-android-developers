73b4c4dfc848b89ea08df9fbff17ef7890ff334c
KODAK <linhtt.it@gmail.com>
2019-07-02 17:57:44 +0700

    Part 1 - Chapter 1: Android Live Search example: Coffee break exercise
    Add switch button to track state change & warning text view by the length

--- android-live-search-example/app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
+++ android-live-search-example/app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.widget.RxCompoundButton;
 import com.jakewharton.rxbinding2.widget.RxTextView;
 
 import java.util.ArrayList;
@@ -27,13 +30,23 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.search_results);
         listView.setAdapter(arrayAdapter);
 
+        Switch switchButton = (Switch) findViewById(R.id.switch_button);
         EditText editText = (EditText) findViewById(R.id.edit_text);
+        TextView warningTextView = (TextView) findViewById(R.id.warning_text);
 
         RxTextView.textChanges(editText)
             .filter(text -> text.length() >= 3)
             .debounce(150, TimeUnit.MILLISECONDS)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(this::updateSearchResults);
+
+        RxCompoundButton.checkedChanges(switchButton)
+            .subscribe(isChecked -> {
+                editText.setText(isChecked ? "box checked!" : ":(");
+            });
+
+        RxTextView.textChanges(editText)
+            .subscribe(text -> warningTextView.setText(text.length() > 7 ? "text too long!" : ""));
     }
 
     private void clearSearchResults() {
